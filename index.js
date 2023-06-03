5c7ce7a61be7506f60ba39ff23da41f3e7ad4e8b
Rakibul Islam <rakib011110@gmail.com>
2023-06-03 17:13:37 +0600

    addmin api add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //  middle ware 
 app.use(cors())
@@ -28,12 +28,50 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const usersCollection = client.db("bistroDB").collection("users");
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
 
 
 
+
+        // here is User relatate APi 
+        app.get("/users", async (req, res) => {
+            const user = await usersCollection.find().toArray()
+            res.send(user)
+        })
+
+        app.post("/users", async (req, res) => {
+            const users = req.body
+
+            const queary = { email: users.email }
+            const existingEmail = await usersCollection.findOne(queary)
+            console.log("existing User", existingEmail);
+            if (existingEmail) {
+                return res.send({ message: "user allready added" })
+            }
+            const result = await usersCollection.insertOne(users)
+            res.send(result)
+        })
+
+        //  creating addmin api  
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+        /* -------------------- */
+
+        // here is menu items API
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
@@ -43,21 +81,44 @@ async function run() {
             const result = await reviewCollection.find().toArray();
             res.send(result);
         })
+        /* ----------------- */
 
-        //  here is insterting cart collection 
 
 
+        //  here is insterting cart collection post method
         app.post("/carts", async (req, res) => {
             const item = req.body
             const result = await cartCollection.insertOne(item)
             res.send(result)
 
         })
+        //  here is cartCollection apis 
+        //  get data using email 
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        });
+        //  here is delete items 
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
+
 
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!4535");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
